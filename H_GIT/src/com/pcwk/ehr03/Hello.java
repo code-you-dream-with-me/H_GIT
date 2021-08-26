350bc50beca4e69b93087f4115fa1e2460d57316
김보선 <notlelis@gmail.com>
2021-08-26 11:05:02 +0900

    bossss

--- H_GIT/src/com/pcwk/ehr03/Hello.java
+++ H_GIT/src/com/pcwk/ehr03/Hello.java
@@ -3,9 +3,7 @@ package com.pcwk.ehr03;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello,eunsole");
-		System.out.println("Hello,eunsole");
-		
+		System.out.println("Hello java");
 	}
 
 }
