3778c3929c8f85fe21150499caff7bbdeb9d0b56
권보성 <onepiecedr4@gmail.com>
2021-08-26 11:16:34 +0900

    good

---
+++ H_GIT/src/com/pcwk/ehr03/Hello_world.java
@@ -0,0 +1,10 @@
+package com.pcwk.ehr03;
+
+public class Hello_world {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		
+	}
+}
