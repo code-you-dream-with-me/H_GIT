1204fa58b246dd474bf514f7e8252cc9eb498d99
boseong <onepiecedr4@gmail.com>
2021-08-26 11:01:07 +0900

    hello?

---
+++ H_GIT/src/com/pcwk/ehr/Test_12341451.java
@@ -0,0 +1,10 @@
+package com.pcwk.ehr;
+
+public class Test_12341451 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("423.Hello, world!");
+	}
+
+}
