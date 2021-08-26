25d4080741c6d165a32713271ebe26e579486094
thslr8564@gmail.com <wns14@192.168.219.100>
2021-08-26 10:34:11 +0900

    푸쉿

---
+++ H_GIT/src/com/pcwk/ehr/hello01.java
@@ -0,0 +1,13 @@
+package com.pcwk.ehr;
+
+public class hello01 {
+
+	public static void main(String[] args) {
+		System.out.println("01.Hello, world!");
+		System.out.println("02.Hello, world!");
+		System.out.println("03.Hello, world!");
+		System.out.println("04.Hello, world!");
+
+	}
+
+}
