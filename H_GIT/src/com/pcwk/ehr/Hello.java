549446588fb45b3175266d970426a0c3add43333
hykim22@gmail.com <HKEDU@192.168.3.100>
2021-08-26 10:17:10 +0900

    init

--- H_GIT/src/com/pcwk/ehr/Hello.java
+++ H_GIT/src/com/pcwk/ehr/Hello.java
@@ -3,8 +3,8 @@ package com.pcwk.ehr;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-
+		System.out.println("01.Hello, world!");
+		System.out.println("02.Hello, world!");
 	}
 
 }
