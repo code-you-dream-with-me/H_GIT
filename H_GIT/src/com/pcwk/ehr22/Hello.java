123522e7046cf158b7a3f1a6fd63f5e6af5fc539
bsgwon <onepiecedr4@gmail.com>
2021-08-26 15:41:42 +0900

    new file

---
+++ H_GIT/src/com/pcwk/ehr22/Hello.java
@@ -0,0 +1,10 @@
+package com.pcwk.ehr22;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("성공을 축하합니다.");
+
+	}
+
+}
---
+++ H_GIT/src/com/pcwk/ehr30/Hello.java
@@ -0,0 +1,9 @@
+package com.pcwk.ehr30;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("성공을 축하합니다.");
+	}
+
+}
