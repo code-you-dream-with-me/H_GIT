c4ac8f374f14b8ba4a64169cb30b7fcb3bbcbee3
heesuu1993@naver.com <HKEDU@192.168.3.118>
2021-08-26 10:45:53 +0900

    I like eunsole! s2 I love u!!

---
+++ H_GIT/src/com/pcwk/ehr02/Hello.java
@@ -0,0 +1,11 @@
+package com.pcwk.ehr02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello,eunsole");
+		System.out.println("Hello,eunsole");
+		
+	}
+
+}
