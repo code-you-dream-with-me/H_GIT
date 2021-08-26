4307fa1808c0bca64602fb01a3c5b46c1f046c93
김현룡 <hykim22@gmail.com>
2021-08-26 11:58:04 +0900

    vipvip

--- H_GIT/src/com/pcwk/ehr/Hello.java
+++ H_GIT/src/com/pcwk/ehr/Hello.java
@@ -3,8 +3,9 @@ package com.pcwk.ehr;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("01.Hello, world!");
-		System.out.println("02.Hello, world!");
+		System.out.println("01.Hello, itdda!");
+		System.out.println("02.Hello, itdda!");
+		System.out.println("03.Hello, itdda!");
 	}
 
 }
