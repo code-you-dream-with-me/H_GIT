f82be324e2749d2da030343d6b0bb9d63ca54c2f
권보성 <onepiecedr4@gmail.com>
2021-08-26 11:13:28 +0900

    hello

---
+++ H_GIT/src/test/go/success/Hello.java
@@ -0,0 +1,11 @@
+package test.go.success;
+
+public class Hello {
+	public static void main(String[] args) {
+	
+	
+	System.out.println("01.Hello, world!");
+	System.out.println("02.Hello, world!");
+	
+	}
+}
