e847ca287477622a7fedfefb3e39b0f51de558ba
heesuu1993@naver.com <HKEDU@192.168.3.118>
2021-08-26 11:25:38 +0900

    hesoo

--- H_GIT/src/com/pcwk/ehr02/Test.java
+++ H_GIT/src/com/pcwk/ehr02/Test.java
@@ -1,10 +1,10 @@
 package com.pcwk.ehr02;
 
-public class hello1 {
+public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello,eunsole");
-		
+		// TODO Auto-generated method stub
+
 	}
 
 }
