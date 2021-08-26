d9b9a83a4b026fccb594d6c55b892b4fdb8f85bd
ibee <ibee@ibee-PC>
2021-08-26 11:01:00 +0900

    bos

---
+++ H_GIT/src/com/pcwk/ehr03/Hello.java
@@ -0,0 +1,11 @@
+package com.pcwk.ehr03;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello,eunsole");
+		System.out.println("Hello,eunsole");
+		
+	}
+
+}
